32e24d244f22b26eb105177cd2266011252c26b2
Kaheru <kaherubrian2016@gmail.com>
2025-03-25 00:48:42 +0300

    Started a jdnago development environment and created the models with a custome user

--- Inventory_api/capston_api/models.py
+++ Inventory_api/capston_api/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+# import the abstract user model 
+from django.contrib.auth.models import AbstractUser
+#  create a custome user model and add roles
+class CustomUser(AbstractUser):
+    pass
 
-# Create your custom user models here
+# create inventory models
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
 
